c162b58d76e6474fc645a113bd5c3882c0f42469
ahmed <ahmed@DESKTOP-AL090QP>
2026-05-19 13:18:58 +0300

    .

--- Tajnned.Infrastructure/Dapper/Helper.cs
+++ Tajnned.Infrastructure/Dapper/Helper.cs
@@ -1,5 +1,6 @@
 ﻿using Dapper;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Data;
 using System.Text;
@@ -18,6 +19,95 @@ namespace Tajnned.Infrastructure.Dapper
             {
                 var value = prop.GetValue(obj);
 
+                if (prop.PropertyType == typeof(string))
+                {
+                    parameters.Add("@" + prop.Name, value);
+                    continue;
+                }
+
+                parameters.Add("@" + prop.Name, value);
+            }
+
+            return parameters;
+        }
+
+
+        public static DynamicParameters ToDynamicParam<T>(this T obj)
+        {
+            var parameters = new DynamicParameters();
+
+            if (obj == null)
+                return parameters;
+
+            var props = typeof(T).GetProperties();
+
+            foreach (var prop in props)
+            {
+                var value = prop.GetValue(obj);
+
+                if (value == null)
+                {
+                    parameters.Add("@" + prop.Name, null);
+                    continue;
+                }
+
+                // skip string (because it's IEnumerable<char>)
+                if (prop.PropertyType == typeof(string))
+                {
+                    parameters.Add("@" + prop.Name, value);
+                    continue;
+                }
+
+                // TVP case (List<T>)
+                if (value is IEnumerable enumerable &&
+                    prop.PropertyType.IsGenericType)
+                {
+                    var elementType = prop.PropertyType.GetGenericArguments()[0];
+
+                    // int TVP
+                    if (elementType == typeof(int))
+                    {
+                        parameters.Add("@" + prop.Name,
+                            ((IEnumerable<int>)value)
+                                .ToTvp("dbo." + prop.Name + "TVP"));
+                        continue;
+                    }
+
+                    // string TVP
+                    if (elementType == typeof(string))
+                    {
+                        parameters.Add("@" + prop.Name,
+                            ((IEnumerable<string>)value)
+                                .ToTvp("dbo." + prop.Name + "TVP"));
+                        continue;
+                    }
+                }
+
+                // normal scalar
+                parameters.Add("@" + prop.Name, value);
+            }
+
+            return parameters;
+        }
+
+
+        public static DynamicParameters ToDynamicParamsWithTvp<T>(this T obj)
+        {
+            var parameters = new DynamicParameters();
+
+            var props = typeof(T).GetProperties();
+
+            foreach (var prop in props)
+            {
+                var value = prop.GetValue(obj);
+
+                if (value == null)
+                {
+                    parameters.Add("@" + prop.Name, null);
+                    continue;
+                }
+
+                // TVP case (List<int>)
                 if (value is IEnumerable<int> intList && prop.PropertyType != typeof(string))
                 {
                     parameters.Add("@" + prop.Name,
@@ -32,8 +122,20 @@ namespace Tajnned.Infrastructure.Dapper
             return parameters;
         }
 
+        public static SqlMapper.ICustomQueryParameter ToTvp<T>(
+       this IEnumerable<T> values,
+       string tvpTypeName)
+        {
+            var table = new DataTable();
+            table.Columns.Add("Value", typeof(T));
 
-        public static SqlMapper.ICustomQueryParameter ToTvp(
+            foreach (var value in values)
+                table.Rows.Add(value);
+
+            return table.AsTableValuedParameter(tvpTypeName);
+        }
+
+        public static SqlMapper.ICustomQueryParameter ToTvpINT(
                   this IEnumerable<int> values,
                   string tvpTypeName)
         {
@@ -47,7 +149,7 @@ namespace Tajnned.Infrastructure.Dapper
         }
 
     }
-    
-      
-     
+
+
+
 }
